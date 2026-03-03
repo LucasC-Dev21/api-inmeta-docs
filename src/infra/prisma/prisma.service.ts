8f522850d04d7acf6587c2ab6f36c9ec740dc142
Lucas Cesar Silva Oliveira <lucascesardev2023@gmail.com>
2026-03-03 19:10:08 -0300

    feat: adiciona prisma module e prisma service global

---
+++ src/infra/prisma/prisma.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
+
+    if (!databaseUrl) {
+      throw new Error(
+        'DATABASE_URL não foi definida nas variáveis de ambiente',
+      );
+    }
+
+    const pool = new Pool({
+      connectionString: databaseUrl,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
