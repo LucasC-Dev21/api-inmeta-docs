c5aa8d11933b6a651f218fbc3b6ec0ef7a021f2f
Lucas Cesar Silva Oliveira <lucascesardev2023@gmail.com>
2026-03-03 23:47:21 -0300

    feat(infra): implementa prisma collaborator repository e registra como provider token

--- src/infra/prisma/prisma.module.ts
+++ src/infra/prisma/prisma.module.ts
@@ -1,9 +1,16 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaCollaboratorRepository } from './repositories/prisma-collaborator.repository';
+import { COLLABORATOR_REPOSITORY } from '../../shared/constants/repository.tokens';
 
-@Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: COLLABORATOR_REPOSITORY,
+      useClass: PrismaCollaboratorRepository,
+    },
+  ],
+  exports: [COLLABORATOR_REPOSITORY, PrismaService],
 })
 export class PrismaModule {}
